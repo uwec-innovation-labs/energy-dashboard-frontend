17ecb66cbbca3e734c8b8aa636587ed5a802ef89
Steven Nelson <Steven.Nelson96@gmail.com>
2019-02-27 14:33:45 -0600

    Basic form setup.

--- frontend/src/components/Export.js
+++ frontend/src/components/Export.js
@@ -9,10 +9,24 @@ class Export extends Component {
       <div>
       <AppNav />
         <center>
-          This is the export page.
+          <h2>
+            Energy Dashboard Data Export
+          </h2>
+          <p>
+            Data will export in a .CSV format that can be opened in Excel.
+          </p>
+          <content>
+            Choose a building to export data from.
+          <form>
+            <select>
+              <option value="Davies Student Center">Davies Student Center</option>
+              <option value="McIntyre Library">McIntyre Library</option>
+            </select>
+          </form>
+          </content>
         </center>
         <center>
-        <FooterBar />
+          <FooterBar />
         </center>
       </div>
     )
