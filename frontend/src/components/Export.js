771f6ad062c9c30177d9465c5b1ef02424a3e628
Steven Nelson <Steven.Nelson96@gmail.com>
2019-02-27 15:30:00 -0600

    CSS needs tweaks to center form.

--- frontend/src/components/Export.js
+++ frontend/src/components/Export.js
@@ -2,33 +2,53 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 import FooterBar from './FooterBar'
 import AppNav from './AppNav'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  Container,
+  Button,
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
 class Export extends Component {
   render() {
     return (
       <div>
-      <AppNav />
-        <center>
-          <h2>
-            Energy Dashboard Data Export
-          </h2>
-          <p>
-            Data will export in a .CSV format that can be opened in Excel.
-          </p>
-          <content>
-            Choose a building to export data from.
-          <form>
-            <select>
-              <option value="Davies Student Center">Davies Student Center</option>
-              <option value="McIntyre Library">McIntyre Library</option>
-            </select>
-          </form>
-          </content>
-        </center>
-        <center>
-          <FooterBar />
-        </center>
-      </div>
+        <AppNav />
+          <div className="container">
+              <h2>
+                Energy Dashboard Data Export
+              </h2>
+              <p>
+                Data will export in a .CSV format that can be opened in Excel.
+              </p>
+            <Form>
+              <Row>
+                <Label htmlFor="frmNameA">Building</Label>
+                  <Col md={3}>
+                    <FormGroup>
+                      <Input
+                        type="select"
+                        name="building"
+                        id="building"
+                        placeholder="Davies Student Center">
+                          <option value="Davies Student Center">Davies Student Center</option>
+                          <option value="McIntyre Library">McIntyre Library</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+            <center>
+              <FooterBar />
+            </center>
+        </div>
     )
   }
 }
