776561101c400f68217bb8b70d93380c88560478
BrandonPessman <pessman.brandon@gmail.com>
2019-03-04 15:50:06 -0600

    Fixes FooterBar position and adds link to privacy policy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './styles/App.scss'
 import Home from './components/Home'
 import Export from './components/Export'
+import PrivacyPolicy from './components/PrivacyPolicy'
 import Layout from './components/Layout'
 
 class Routes extends Component {
@@ -11,6 +12,7 @@ class Routes extends Component {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/export" exact component={Export} />
+        <Route path="/privacypolicy" exact component={PrivacyPolicy} />
       </Switch>
     )
   }
