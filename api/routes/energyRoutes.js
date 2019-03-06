ff0a083639491a60c3b93d7373245e5aec41bf3c
Laptop <reiterkl9561@uwec.edu>
2019-03-06 14:30:46 -0600

    sorted out routes from server.js

---
+++ api/routes/energyRoutes.js
@@ -0,0 +1,48 @@
+const readData = require("./../CSV/readData.js")
+const sort = require("./sort.js")
+
+//load in data
+var data;
+readData.readData().then(function(result) {
+  data = result;
+});
+var fullData;
+readData.readFullData().then(function(result) {
+  fullData = result;
+});
+var buildings;
+readData.readBuildings().then(function(result) {
+  buildings = result;
+});
+  
+  var getSolar = (parent, args, context, info) => {
+    var newData = data;
+    if (parent != null) {
+      if (parent.sort != null) {
+        //chronological is the default, since that's how the csv file is generated
+        if (parent.sort == "high") {
+          newData = newData.sort(sort.bySolarHigh);     
+        } else if (parent.sort == "low") {
+          newData = newData.sort(sort.bySolarLow);   
+        }
+      }
+      if (parent.only != null && Number.isInteger(parent.only)) {
+        newData = newData.slice(0, parent.only);
+      }
+    }
+    return newData;
+  }
+  
+  var getBuildings = () => {
+    return buildings;
+  }
+  
+  var getFullData = () => {
+  return fullData;
+}
+
+module.exports = {
+    "getSolar": getSolar,
+    "getBuildings": getBuildings,
+    "getFullData": getFullData
+}
--- api/server.js
+++ api/server.js
@@ -3,75 +3,16 @@ const graphqlHTTP = require('express-graphql')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const readData = require("./CSV/readData.js")
+const energyRoutes = require('./routes/energyRoutes.js')
 
 //get data types (Query, Solar, Building, Date)
 var schema = require("./schema.js").energySchema;
 const app = express();
 
-//load in data
-var data;
-readData.readData().then(function(result) {
-  console.log("here " + result);
-  data = result;
-});
-var fullData;
-readData.readFullData().then(function(result) {
-  fullData = result;
-});
-var buildings;
-readData.readBuildings().then(function(result) {
-  buildings = result;
-});
-
-function sortSolarHigh(a, b) {
-  if (a.value > b.value) {
-    return -1;
-  } else if (b.value > a.value) {
-    return 1;
-  }
-  return 0;
-}
-
-function sortSolarLow(a, b) {
-  if (a.value > b.value) {
-    return 1;
-  } else if (b.value > a.value) {
-    return -1;
-  }
-  return 0;
-}
-
-var getSolar = (parent, args, context, info) => {
-  var newData = data;
-  if (parent != null) {
-    if (parent.sort != null) {
-      //chronological is the default, since that's how the csv file is generated
-      if (parent.sort == "high") {
-        newData = newData.sort(sortSolarHigh);     
-      } else if (parent.sort == "low") {
-        newData = newData.sort(sortSolarLow);   
-      }
-    }
-    if (parent.only != null && Number.isInteger(parent.only)) {
-      newData = newData.slice(0, parent.only);
-    }
-  }
-  return newData;
-}
-
-var getBuildings = () => {
-  return buildings;
-}
-
-var getFullData = () => {
-  return fullData;
-}
-
 var global = {
-  solar: getSolar,
-  buildings: getBuildings,
-  fullData: getFullData
+  solar: energyRoutes.getSolar,
+  buildings: energyRoutes.getBuildings,
+  fullData: energyRoutes.getFullData
 }
 
 app.use(
