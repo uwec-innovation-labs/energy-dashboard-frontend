5e90444d80819ba32d9fd242c98d58b9225abe1d
Laptop <reiterkl9561@uwec.edu>
2019-03-13 14:17:19 -0500

    Ready for pull request

--- api/server.js
+++ api/server.js
@@ -10,7 +10,38 @@ var schema = require("./sqlSchema.js").energySchema;
 const app = express();
 
 var global = {
-  solar: sqlData.getSolar,
+  Davies: sqlData.master,
+  Schneider: sqlData.master,
+  Chancellors: sqlData.master,
+  Crest: sqlData.master,
+  Governors: sqlData.master,
+  Horan: sqlData.master,
+  Hibbard: sqlData.master,
+  Hilltop: sqlData.master,
+  HSS: sqlData.master,
+  McPhee: sqlData.master,
+  TowersSouth: sqlData.master,
+  ED: sqlData.master,
+  Bridgman: sqlData.master,
+  Centennial: sqlData.master,
+  KV: sqlData.master,
+  HFANorth: sqlData.master,
+  HFASouth: sqlData.master,
+  HeatingPlant: sqlData.master,
+  Library: sqlData.master,
+  Maintenance: sqlData.master,
+  PhillipsNorth: sqlData.master,
+  PhillipsSouth: sqlData.master,
+  Nursing: sqlData.master,
+  Murray: sqlData.master,
+  Schofield: sqlData.master,
+  Putnam: sqlData.master,
+  OakRidge: sqlData.master,
+  Sutherland: sqlData.master,
+  Thomas: sqlData.master,
+  Zorn: sqlData.master,
+  tables: sqlData.getTables,
+  test: sqlData.test
 }
 
 app.use(
--- api/sqlData.js
+++ api/sqlData.js
@@ -2,75 +2,87 @@ const sort = require("./routes/sort.js")
 const sqlserver = require("./sqlConnect.js")
 const sql = require('mssql')
 
-var newQuery = 'SELECT TOP 10 * FROM dbo.SCHNEIDER_HALL_LIBSOLTOTALYIELD;'
-var solarQuery = 'SELECT TIMESTAMP AS timestamp, VALUE as value FROM dbo.SCHNEIDER_HALL_LIBSOLTOTALYIELD'
-var dateQuery = 'SELECT TOP 5 TIMESTAMP AS timestamp, VALUE as value FROM dbo.SCHNEIDER_HALL_LIBSOLTOTALYIELD WHERE CAST(TIMESTAMP as DATETIME) < CAST(@date as DATETIME);';
+var whereClauses;
+var parameters;
 
-async function getSolar(parent, args , context, info) {
-  var localSolarQuery = solarQuery;
-  //parameters passed in by GraphQL
-  if (parent != null) {
-    //We will add all the 'where' clauses to the SQL statment at the end
-    whereClauses = [];
-    //Input parameters need to be passed along with the query
-    parameters = [];
-    
-    //'only': selects first n records
-    if (parent.only != null) {
-      localSolarQuery = localSolarQuery.slice(0, 6) + " TOP " + parent.only + localSolarQuery.slice(6);
-      parameters.push({
-        name: 'top',
-        value: parent.top
-      })
-    }
+async function test(parent, args , context, info) {
+  var newQuery = 'SELECT * FROM ' + testTable + ' WHERE VALUE < 0';
+  let returnData = await sqlserver.getSQLData(newQuery, []);
+  console.log(returnData);
+}
 
-    //'start': earliest date
-    if (parent.start != null) {
-      whereClauses.push("CAST(TIMESTAMP AS DATETIME) > CAST(@start AS DATETIME)");
-      parameters.push({
-        name: 'start',
-        value: new Date(parent.start)
-      });
-    }
+async function getHistoryConfig(parent, args , context, info) {
+  var newQuery = 'SELECT * FROM dbo.HISTORY_CONFIG';
+  let returnData = await sqlserver.getSQLData(newQuery, []);
+  return returnData;
+}
 
-    //'end': latest date
-    if (parent.end != null) {
-      whereClauses.push("CAST(TIMESTAMP AS DATETIME) > CAST(@end AS DATETIME)");
-      parameters.push({
-        name: 'end',
-        value: new Date(parent.end)
-      });
-    }
+async function master(parent, args, context, info) {
+  var building = context.fieldName;
+  if (parent.average != null) {
+    return average(parent, building);
+  } else {
+    return select(parent, building)
+  }
+}
 
-    if (whereClauses.length > 0) {
-      localSolarQuery += " WHERE " + whereClauses[0];
-      var i = 1;
-      while (i < whereClauses.length) {
-        localSolarQuery += " AND " + whereClauses[i];
-      }
-    }
+async function average(parent, building) {
+  var averageBy = "DATEPART(year, TIMESTAMP)";
+  if (parent.average == "month") {
+    averageBy += ", DATEPART(month, TIMESTAMP)";
+  }
+  var avgQuery = "SELECT DATEPART(year, TIMESTAMP) as year, ";
+  if (parent.average == "month") {
+    avgQuery += "DATEPART(month, TIMESTAMP) as month, ";
+  }
+  avgQuery += "AVG(VALUE) as value FROM ";
 
-    //'sort': sorts by timestamp or value, asc or desc
-    if (parent.sort != null) {
-      if (parent.sort == "value high") {
-        localSolarQuery += " ORDER BY VALUE DESC";
-      } else if (parent.sort == "value low") {
-        localSolarQuery += " ORDER BY VALUE ASC";
-      } else if (parent.sort == "timestamp high") {
-        localSolarQuery += " ORDER BY TIMESTAMP DESC"
-      } else if (parent.sort == "timestamp low") {
-        localSolarQuery += " ORDER BY TIMESTAMP ASC"
-      }
+  var avgSort = parent.sort;
+  parent.sort = null;
+  avgQuery = queryBuilder(avgQuery, parent, building);
+  avgQuery += " GROUP BY " + averageBy;
+
+  if (avgSort != null) {
+    if (avgSort == "value high") {
+      avgQuery += " ORDER BY AVG(VALUE) DESC";
+    } else if (avgSort == "value low") {
+      avgQuery += " ORDER BY AVG(VALUE) ASC";
+    } else if (avgSort == "timestamp high") {
+      avgQuery += " ORDER BY DATEPART(year, TIMESTAMP) DESC, DATEPART(month, TIMESTAMP) DESC"
+    } else if (avgSort == "timestamp low") {
+      avgQuery += " ORDER BY DATEPART(year, TIMESTAMP) ASC, DATEPART(month, TIMESTAMP) ASC"
     }
   }
+  console.log(avgQuery);
+  let returnData = await sqlserver.getSQLData(avgQuery, parameters);
+  returnData.forEach(function(data) {
+    data.timestamp = {
+      year: data.year,
+      month: data.month,
+    }
+  });
+  return returnData;
+}
 
-  console.log(localSolarQuery);
-  let returnData = await sqlserver.getSQLData(localSolarQuery, parameters);
+async function getTables(parent, args, context, info) {
+  var tableQuery = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES';
+  var tableNames = [];
+  let returnData = await sqlserver.getSQLData(tableQuery, []);
+  returnData.forEach(function(data) {
+    tableNames.push(data.TABLE_NAME);
+  });
+  return tableNames;
+}
+
+async function select(parent, building) {
+  var solarQuery = 'SELECT TIMESTAMP AS timestamp, VALUE as value FROM ';
+  solarQuery = queryBuilder(solarQuery, parent, building);  
+  let returnData = await sqlserver.getSQLData(solarQuery, parameters);
   returnData.forEach(function(data) {
     var fullDate = new Date(data.timestamp);
     data.timestamp = {
         "year": fullDate.getFullYear(),
-        "month": fullDate.getMonth(),
+        "month": fullDate.getMonth() + 1,
         "day": fullDate.getDate(),
         "hour": fullDate.getHours(),
         "minute": fullDate.getMinutes(),
@@ -83,6 +95,213 @@ async function getSolar(parent, args , context, info) {
   return returnData;
 }
 
+function queryBuilder(query, parent, building) {
+  whereClauses = [];
+  parameters = [];
+  //get building you want data from
+  if (building == "Davies") {
+    if (parent.dataType == "energyRate") {
+      query += "dbo.WRDAVIES_NC1_ENERGYRATE";
+    } else if (parent.dataType == "heat") {
+      query += "dbo.WRDAVIES_NC1_CONDENSATE_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_DAVIESKW_TOT_VALUE";
+    } 
+  } else if (building == "Schneider") {
+    if (parent.dataType == "solar") {
+      query += "dbo.SCHNEIDER_HALL_LIBSOLTOTALYIELD";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_SCHNEIDERKW_TOT_VALUE";
+    } 
+  } else if (building == "Governors") {
+    if (parent.dataType == "heat") {
+      query += "dbo.UWEC_GOVERNORS_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_GOVERNORSKW_TOT_VALUE";
+    } 
+  } else if (building == "Chancellors") {
+    if (parent.dataType == "heat") {
+      query += "dbo.CHANCELLORS_CHANCELLORS_CONDMTR_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_CHANCELLORSKW_TOT_VALUE";
+    } else if (parent.dataType == "chillers") {
+      query += "dbo.SCHNEIDER_HALL_CHANCELLORS_CHILLERKW_TOT_VALUE";
+    }
+  } else if (building == "Horan") {
+    if (parent.dataType == "heat") {
+      query += "dbo.UWEC_HORAN_CONDYESTERDAY";
+    } else if (parent.dataType == "chiller") {
+      query += "dbo.SCHNEIDER_HALL_HORANKW_TOT_VALUE";
+    } 
+  } else if (building == "Crest") {
+    if (parent.dataType == "heat") {
+      query += "dbo.CREST_CWC_CONDENSATE_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_CREST_COMMONSKWH_TOT_VALUE";
+    }
+  } else if (building == "Hibbard") {
+    if (parent.dataType == "heat") {
+      query += "dbo.HIBBARD_CONDENSATE_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HIBBARDKW_TOT_VALUE";
+    }
+  } else if (building == "Hilltop") {
+    if (parent.dataType == "heat") {
+      query += "dbo.HILLTOP_LL_MISC_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HILLTOPKW_TOT_VALUE";
+    } else if (parent.dataType == "chiller") {
+      query += "dbo.SCHNEIDER_HALL_HILLTOP_CHILLERKW_TOT_VALUE";
+    } 
+  } else if (building == "HSS") {
+    if (parent.dataType == "heat") {
+      query += "dbo.UWEC_HSS_CONDMTR_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HSSKW_TOT_VALUE";
+    }
+  } else if (building == "McPhee") {
+    if (parent.dataType == "heat") {
+      query += "dbo.MCPHEE_NATATORIUM_CONDENSATE_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_MCPHEEKW_TOT_VALUE";
+    } 
+  } else if (building == "TowersSouth") {
+    if (parent.dataType == "heat") {
+      query += "dbo.UWEC_TOWERSSOUTH3RD_HWCONV_CONDYESTERDAY";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_TOWERS_SOUTHKW_TOT_VALUE";
+    } 
+  } else if (building == "ED") {
+    if (parent.dataType == "heat") {
+      query += "dbo.ED_BLDG_2ND_FL_JENE_CONDENSATEMETER_CONDYESTERDAY";
+    }
+  } else if (building == "Bridgman") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_BRIDGEMANKW_TOT_VALUE";
+    }
+  } else if (building == "Centennial") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_CENTENNIAL_HALLKW_TOT_VALUE";
+    }
+  } else if (building == "KV") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_5KV_MAINKW_TOT_VALUE";
+    }
+  } else if (building == "HFANorth") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HFA_NORTHKW_TOT_VALUE";
+    }
+  } else if (building == "HFASouth") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HFA_SOUTHKW_TOT_VALUE";
+    }
+  } else if (building == "HeatingPlant") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_HEATING_PLANTKW_TOT_VALUE";
+    }
+  } else if (building == "Library") {
+    if (parent.dataType == "chiller") {
+      query += "dbo.SCHNEIDER_HALL_LIBRARY_CHILLERKW_TOT_VALUE";
+    } else if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_LIBRARY_BLDG_ONLYKW_TOT_VALUE";
+    }
+  } else if (building == "Maintenance") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_MAINT_BLDGKW_TOT_VALUE";
+    }
+  } else if (building == "PhillipsNorth") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_PHILLIPS_NORTHKW_TOT_VALUE";
+    }
+  } else if (building == "PhillipsSouth") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_PHILLIPS_SOUTHKW_TOT_VALUE";
+    }
+  } else if (building == "Nursing") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_NURSINGKW_TOT_VALUE";
+    }
+  } else if (building == "Murray") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_MURRAYKW_TOT_VALUE";
+    }
+  } else if (building == "Schofield") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_SCHOFIELDKW_TOT_VALUE";
+    }
+  } else if (building == "Putnam") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_PUTNAMKW_TOT_VALUE";
+    }
+  } else if (building == "OakRidge") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_OAK_RIDGEKW_TOT_VALUE";
+    }
+  } else if (building == "Sutherland") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_SUTHERLANDKW_TOT_VALUE";
+    }
+  } else if (building == "Thomas") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_THOMASKW_TOT_VALUE";
+    }
+  } else if (building == "Zorn") {
+    if (parent.dataType == "energy") {
+      query += "dbo.SCHNEIDER_HALL_ZORNKW_TOT_VALUE";
+    }
+  }
+
+  //'only': selects first n records
+  if (parent.only != null) {
+    query = query.slice(0, 6) + " TOP " + parent.only + query.slice(6);
+  }
+
+  //'start': earliest date
+  if (parent.start != null) {
+    whereClauses.push("CAST(TIMESTAMP AS DATETIME) > CAST(@start AS DATETIME)");
+    parameters.push({
+      name: 'start',
+      value: new Date(parent.start)
+    });
+  }
+
+  //'end': latest date
+  if (parent.end != null) {
+    whereClauses.push("CAST(TIMESTAMP AS DATETIME) < CAST(@end AS DATETIME)");
+    parameters.push({
+      name: 'end',
+      value: new Date(parent.end)
+    });
+  }
+
+  if (whereClauses.length > 0) {
+    query += " WHERE " + whereClauses[0];
+    var i = 1;
+    while (i < whereClauses.length) {
+      query += " AND " + whereClauses[i];
+      i++;
+    }
+  }
+
+  //'sort': sorts by timestamp or value, asc or desc
+  if (parent.sort != null) {
+    if (parent.sort == "value high") {
+      query += " ORDER BY VALUE DESC";
+    } else if (parent.sort == "value low") {
+      query += " ORDER BY VALUE ASC";
+    } else if (parent.sort == "timestamp high") {
+      query += " ORDER BY TIMESTAMP DESC"
+    } else if (parent.sort == "timestamp low") {
+      query += " ORDER BY TIMESTAMP ASC"
+    }
+  }
+  console.log(query);
+  return query;
+}
+
 module.exports = {
-  "getSolar": getSolar
+  "getTables": getTables,
+  "test": test,
+  "master": master,
+  "history": getHistoryConfig
 }
