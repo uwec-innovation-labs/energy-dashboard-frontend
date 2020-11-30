1f686c1abbd53413b679a2ba15c2ded31cdc554e
brandonpessman <pessman.brandon@gmail.com>
2020-11-30 14:15:05 -0600

    Code changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,32 @@ import SolarIcon from '@material-ui/icons/Brightness5Outlined';
 import WaterIcon from '@material-ui/icons/Opacity';
 import TranslateIcon from '@material-ui/icons/Translate';
 
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import axios from 'axios'
+
 function App() {
   const [building, setBuilding] = useState('Library');
   const [energyType, setEnergyType] = useState('Electricity')
-  const [energyTypes, setEnergyTypes] = useState([])
+  const [buildings, setBuildings] = useState([])
   //const [currentTranslation, setCurrentTranslation] = useState("English")
   const [translationModal, setTranslationModal] = useState(false)
+  const [energySubType, setEnergySubType] = useState('kw')
+  const [x, setX] = useState([])
+  const [y, setY] = useState([])
 
   useEffect(() => {
     // Make API call to get buildings
     // Make API call to get energy type of all the buildings
-    const arr = ["Library", "Davies", "Phillips"]
-    setEnergyTypes(arr);
+    const arr = ["Library", "Davies", "Phillips", "Haas"]
+    setBuildings(arr);
+    makeApiCall(arr[0], 'kw');
   }, [])
 
   const handleBuildingChange = (event) => {
@@ -44,6 +53,77 @@ function App() {
     setTranslationModal(!translationModal)
   }
 
+  const handleRadioChange = (event) => {
+    setEnergySubType(event.target.value)
+    makeApiCall(building, event.target.value)
+  }
+
+  const makeApiCall = (whichBuilding, whichSubType) => {
+    axios.post('http://40.77.105.196:8080/query', {
+      query: `
+      query {
+       dashboardHomePage {
+         campusKW {
+           data {
+             value
+             building
+             dateTimeUnix
+             unit
+             type
+           }
+           errors {
+             error
+             errors
+           }
+         }
+         campusKWH {
+           data {
+             value
+             building
+             dateTimeUnix
+             unit
+             type
+           }
+           errors {
+             error
+             errors
+           }
+         }
+       }
+     }
+         `
+   }
+     ).then((result) => {
+        let data;
+        console.log(result)
+         if (whichSubType === "kw") {
+          data = result.data.data.dashboardHomePage.campusKW.data
+         } else {
+          data = result.data.data.dashboardHomePage.campusKWH.data
+         }
+         
+         
+
+         let d = data;
+         //const d = dnotsorted.sort((a, b) => a.dateTimeUnix - b.dateTimeUnix)
+ 
+         let i;
+         let x = [];
+         let y = [];
+         let j = 0;
+         for (i = 0; i < d.length; i++) {
+            if (d[i].value < 5000 && j  % 15 == 0) {
+              x.push(new Date(d[i].dateTimeUnix * 1000));
+              y.push(d[i].value)
+            }
+            j++;
+         }
+ 
+         setX(x)
+         setY(y)
+       });
+  }
+
   return (
     <>
       <TranslateIcon style={{ fill: 'white', backgroundColor: 'darkgray', padding: '10px', marginLeft: '30px', borderRadius: '4px', float: 'right' }} onClick={handleOpenTranslations} />
@@ -51,15 +131,15 @@ function App() {
       <Grid container spacing={8} style={{ height: '400px' }}>
         <Grid item xs={8}>
           <div className="paper">
-            <LineChart building={building} energyType={energyType} />
+            <LineChart building={building} energyType={energyType} x={x} y={y} />
           </div>
         </Grid>
         <Grid item xs={4}>
           <div className="paper">
             <h1>Dashboard Tools</h1>
             <br />
+            <h2>Building</h2>
             <FormControl style={{ width: '100%', marginBottom: '10px' }}>
-              <InputLabel shrink>Building</InputLabel>
               <Select
                 value={building}
                 onChange={handleBuildingChange}
@@ -68,17 +148,31 @@ function App() {
                 <MenuItem value="">
                   <em>Select a Building</em>
                 </MenuItem>
-                {energyTypes.map((type) => <MenuItem value={type}>{type}</MenuItem>)}
+                {buildings.map((type) => <MenuItem value={type}>{type}</MenuItem>)}
               </Select>
             </FormControl>
             {building ? (<>
-              <h2>Energy Type</h2>
-              <IconButton Icon={ElectricityIcon} handleEnergyChange={handleEnergyChange} type={'Electricity'} />
-              <IconButton Icon={SolarIcon} handleEnergyChange={handleEnergyChange} type={'Solar'} />
-              <IconButton Icon={WaterIcon} handleEnergyChange={handleEnergyChange} type={'Water'} isDisabled={true} />
+              <h2 style={{marginTop: '15px'}}>Energy Type</h2>
+              <IconButton Icon={ElectricityIcon} handleEnergyChange={handleEnergyChange} type={'Electricity'} whatSelected={energyType} />
+              <IconButton Icon={SolarIcon} handleEnergyChange={handleEnergyChange} type={'Solar'} whatSelected={energyType} />
+              <IconButton Icon={WaterIcon} handleEnergyChange={handleEnergyChange} type={'Water'} whatSelected={energyType} isDisabled={true} />
               <br />
               <br />
               <br />
+              {energyType === "Electricity" ? 
+              <>
+                <h2 style={{marginTop: '15px'}}>Sub Type</h2>
+                <FormControl component="fieldset">
+                  <RadioGroup aria-label="gender" name="gender1" value={energySubType} onChange={handleRadioChange}>
+                    <FormControlLabel value="kw" control={<Radio />} label="kw" />
+                    <FormControlLabel value="kw/hr" control={<Radio />} label="kw/hr" />
+                  </RadioGroup>
+                </FormControl>
+                <br />
+                <br />
+              </>
+              : ''
+              }
               <h2>Functions</h2>
               <center>
                 <Button text="Export Data" />
@@ -88,6 +182,24 @@ function App() {
             }
           </div>
         </Grid>
+        <Grid item xs={4} style={{marginBottom: '100px'}}>
+            <div className="paper">
+              <h1>About the Student Office of Sustainability</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan lacus tellus, vitae tincidunt metus sodales ac. Integer in molestie diam, nec elementum nunc. Morbi vel egestas nisi. Etiam rutrum bibendum eros, vel auctor tellus maximus eu. Duis quis tristique orci. Sed sed felis enim. Pellentesque suscipit placerat quam vitae pretium. Mauris laoreet in mi tristique pulvinar. Donec commodo lacinia velit, ut rutrum sapien facilisis nec. Quisque at viverra massa. Nam et ipsum vestibulum, semper libero quis, egestas quam.</p>
+            </div>
+          </Grid>
+          <Grid item xs={4} style={{marginBottom: '100px'}}>
+            <div className="paper">
+              <h1>About the Innovation Labs</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan lacus tellus, vitae tincidunt metus sodales ac. Integer in molestie diam, nec elementum nunc. Morbi vel egestas nisi. Etiam rutrum bibendum eros, vel auctor tellus maximus eu. Duis quis tristique orci. Sed sed felis enim. Pellentesque suscipit placerat quam vitae pretium. Mauris laoreet in mi tristique pulvinar. Donec commodo lacinia velit, ut rutrum sapien facilisis nec. Quisque at viverra massa. Nam et ipsum vestibulum, semper libero quis, egestas quam.</p>
+            </div>
+          </Grid>
+          <Grid item xs={4} style={{marginBottom: '100px'}}>
+            <div className="paper">
+              <h1>About the SOS Energy Dashboard</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan lacus tellus, vitae tincidunt metus sodales ac. Integer in molestie diam, nec elementum nunc. Morbi vel egestas nisi. Etiam rutrum bibendum eros, vel auctor tellus maximus eu. Duis quis tristique orci. Sed sed felis enim. Pellentesque suscipit placerat quam vitae pretium. Mauris laoreet in mi tristique pulvinar. Donec commodo lacinia velit, ut rutrum sapien facilisis nec. Quisque at viverra massa. Nam et ipsum vestibulum, semper libero quis, egestas quam.</p>
+            </div>
+          </Grid>
       </Grid>
     </>
   )
--- frontend/src/components/Atoms/IconButton.js
+++ frontend/src/components/Atoms/IconButton.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function IconButton({ Icon, isDisabled, handleEnergyChange, type }) {
+function IconButton({ Icon, isDisabled, handleEnergyChange, type, whatSelected }) {
     return (
         <>
-            <div className={isDisabled ? "icon-disabled" : "icon"} onClick={() => isDisabled ? '' : handleEnergyChange(type)}>
+            <div className={isDisabled ? "icon-disabled" : (whatSelected === type ? "icon-selected" : "icon")} onClick={() => isDisabled ? '' : handleEnergyChange(type)}>
                 <center>
                     <Icon fontSize="large" style={{ fill: "white" }} />
                 </center>
